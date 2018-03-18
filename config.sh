dbf2101113e9c5a6a1dd36c4e9bb3e19d07cce40
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2018-03-18 15:58:51 +0100

    MAINT add appveyor and reduce matrix for osx

--- config.sh
+++ config.sh
@@ -7,6 +7,18 @@ function pre_build {
     :
 }
 
+function before_install {
+    # Uninstall oclint. See Travis-CI gh-8826
+    brew cask uninstall oclint || true
+    # Force to use GCC for open-mp support
+    brew install gcc
+    export CC=g++-7
+    export CXX=g++-7
+    get_macpython_environment $MB_PYTHON_VERSION venv
+    source venv/bin/activate
+    pip install --upgrade pip wheel
+}
+
 function run_tests {
     # Runs tests on installed distribution from an empty directory
     python --version
